ad9e265c727e0c9cdf11e737cd8b662d8246e8e2
Kevin Clark <kevinwilliamclark@gmail.com>
2021-04-18 15:10:52 -0500

    Adds form to create, and delete a resolution

--- resolutions/client/main.js
+++ resolutions/client/main.js
@@ -1,22 +1,34 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Mongo } from 'meteor/mongo';
+
+Resolutions = new Mongo.Collection('resolutions');
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.body.helpers({
+  resolutions: function() {
+    return Resolutions.find()
+  }
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.body.events({
+  'submit .new-resolution': function(event) {
+    let title = event.target.title.value
+    console.log(event)
+
+    Resolutions.insert({
+      title: title,
+      createAt: new Date()
+    });
+    event.target.title.value = "";
+    return false
+  }
+
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+Template.resolution.events({
+  'click .delete': function() {
+    Resolutions.remove(this._id);
+  }
+})
