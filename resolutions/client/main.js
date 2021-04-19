df59323ea57c19a495704c8bc4f1f54534ffee6d
Kevin Clark <kevinwilliamclark@gmail.com>
2021-04-19 09:41:51 -0500

    Adds a hideFinished variable to the session.
    Allows the user to hide checked tasks

--- resolutions/client/main.js
+++ resolutions/client/main.js
@@ -2,13 +2,21 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Mongo } from 'meteor/mongo';
 
+
 Resolutions = new Mongo.Collection('resolutions');
 
 import './main.html';
 
 Template.body.helpers({
   resolutions: function() {
-    return Resolutions.find()
+    if(Session.get('hideFinished')) {
+      return Resolutions.find({checked: {$ne: true}});
+    } else {
+      return Resolutions.find()
+    }
+  },
+  hideFinished: function() {
+    return Session.get('hideFinished')
   }
 });
 
@@ -23,12 +31,27 @@ Template.body.events({
     });
     event.target.title.value = "";
     return false
+  },
+  'change .hide-finished': function(event) {
+    Session.set('hideFinished', event.target.checked);
   }
 
 });
 
 Template.resolution.events({
+  'click .toggle-checked': function() {
+    Resolutions.update(this._id, {$set: {checked: !this.checked}})
+  },
   'click .delete': function() {
     Resolutions.remove(this._id);
   }
 })
+
+// Meteor.methods({
+//   addResolution: function(title){ 
+//     Resolutions.insert({
+//       title: title,
+//       createAt: new Date()
+//     });
+//   }
+// })
